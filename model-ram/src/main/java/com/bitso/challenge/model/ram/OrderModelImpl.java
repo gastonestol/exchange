478215056929d992578f4ff3b3cad659f6210b5f
Gaston Estol <gastonestol@Gastons-Air.fibertel.com.ar>
2021-02-15 19:08:28 -0300

    implementing findOrdersForBook

--- model-ram/src/main/java/com/bitso/challenge/model/ram/OrderModelImpl.java
+++ model-ram/src/main/java/com/bitso/challenge/model/ram/OrderModelImpl.java
@@ -50,6 +50,8 @@ public class OrderModelImpl implements OrderModel {
 
     @Override
     public List<Order> findOrdersForBook(Currency major, Currency minor) {
-        throw new UnsupportedOperationException("Implement me!");
+        return orders.values().stream()
+                .filter(order -> major == order.getMajor() || minor == order.getMinor())
+                .collect(Collectors.toList());
     }
 }
